776813918896e79a0658f8b47a231f46e1d135de
Andrew Rowls <andrew@eternicode.com>
2015-04-11 11:22:23 -0400

    Style cleanup

--- react.py
+++ react.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python
 
-import os
-import os.path
-from pyinotify import WatchManager, IN_DELETE, IN_CREATE, IN_CLOSE_WRITE, ProcessEvent, Notifier
-import subprocess
-import sys
-import re
 import argparse
 import fnmatch
+import os
+import os.path
+import re
+import subprocess
+import sys
+from pyinotify import (
+    WatchManager, ProcessEvent, Notifier,
+    IN_DELETE, IN_CREATE, IN_CLOSE_WRITE,
+)
+
 
 class PatternAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         setattr(namespace, self.dest, fnmatch.translate(values))
 
-parser = argparse.ArgumentParser(description='Launch a script if specified files change.')
-parser.add_argument('directory', help='the directory which is recursively monitored')
+parser = argparse.ArgumentParser(
+    description="Launch a script if specified files change."
+)
+parser.add_argument(
+    "directory", help="the directory which is recursively monitored"
+)
 
 group = parser.add_mutually_exclusive_group()
-group.add_argument('-r', '--regex', required=False, default=".*", help='files only trigger the reaction if their name matches this regular expression')
-group.add_argument('-p', '--pattern', required=False, dest="regex", action=PatternAction, help='files only trigger the reaction if their name matches this shell pattern')
+group.add_argument(
+    "-r", "--regex",
+    required=False,
+    default=".*",
+    help=(
+        "Files only trigger the reaction if their name matches this regular "
+        "expression"
+    )
+)
+group.add_argument(
+    "-p", "--pattern",
+    required=False,
+    dest="regex",
+    action=PatternAction,
+    help=(
+        "Files only trigger the reaction if their name matches this shell "
+        "pattern"
+    )
+)
 
 parser.add_argument("script", help="the script that is executed upon reaction")
 
+
 class Options:
-    __slots__=["directory", "regex", "script"]
+    __slots__ = ["directory", "regex", "script"]
 
 options = Options()
 args = parser.parse_args(namespace=options)
 
-class Reload (Exception):
+
+class Reload(Exception):
     pass
+
 
 class Process(ProcessEvent):
     def __init__(self,  options):
@@ -47,7 +75,7 @@ class Process(ProcessEvent):
     def process_IN_CLOSE_WRITE(self, event):
         target = os.path.join(event.path, event.name)
         if self.regex.match(target):
-            args = self.script.replace('$f', target).split()
+            args = self.script.replace("$f", target).split()
             os.system("clear")
             sys.stdout.write("executing script: " + " ".join(args) + "\n")
             subprocess.call(args)
